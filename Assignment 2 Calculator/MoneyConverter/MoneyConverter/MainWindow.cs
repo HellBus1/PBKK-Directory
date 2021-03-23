dcdd3e035881c1cee1f82254a4c7a4f0a08b3b40
HellBus1 <syubbanfakhriya@gmail.com>
2021-03-23 23:57:02 +0700

    adding layout

---
+++ Assignment 2 Calculator/MoneyConverter/MoneyConverter/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        Initialize();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    private void Initialize()
+    {
+
+    }
+}
