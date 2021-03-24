255b41b0c883b407c69c324b0d4ebea3aad362f4
HellBus1 <syubbanfakhriya@gmail.com>
2021-03-24 22:12:59 +0700

    add currency model

---
+++ Assignment 2 Calculator/MoneyConverter/MoneyConverter/Model/CurrenciesCode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace MoneyConverter.Model
+{
+    public class CurrenciesCode
+    {
+        private String codeName;
+        private Currency currency;
+
+        CurrenciesCode(String codeName, Currency currency)
+        {
+            this.codeName = codeName;
+            this.currency = currency;
+        }
+
+        public String GetCodename()
+        {
+            return this.codeName;
+        }
+
+        public Currency GetCurrencies()
+        {
+            return this.currency;
+        }
+    }
+}
