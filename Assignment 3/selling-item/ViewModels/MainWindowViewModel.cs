9f9e20d1670a8756aed90f6084f1c1d65ae13c41
HellBus1 <syubbanfakhriya@gmail.com>
2021-04-19 01:26:54 +0700

    simple layouting

---
+++ Assignment 3/selling-item/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace selling_item.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        // Window constant 
+        public string NamaBarang => "Nama Barang : ";
+        public string Jumlah => "Jumlah : ";
+        public string Harga => "Harga : ";
+        public string Diskon => "Diskon (%): ";
+        public string Total => "Total : ";
+    }
+}
---
+++ Assignment 3/selling-item/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace selling_item.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
