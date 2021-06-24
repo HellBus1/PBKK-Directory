566a55c27fb405801c335bf21f2218807cf5596e
syubban <syubbanfakhriya@gmail.com>
2021-06-25 01:44:38 +0700

    init codeigniter

---
+++ ci-commerce/app/Views/admin/fragments/app.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?= $this->include('admin/fragments/header') ?>
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+  <div id="wrapper">
+    <div class="preloader flex-column justify-content-center align-items-center">
+      <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    </div>
+    
+    <?= $this->include('admin/fragments/navbar') ?>
+    
+    <?= $this->include('admin/fragments/sidebar') ?>
+
+    <?= $this->renderSection('content') ?>
+    
+  </div>
+  <?= $this->include('admin/fragments/footer') ?>
+</body>
+
+</html>
